732a4f0b6943107fb12e2f1757a99f88f2f77b7c
schaercrowe <karanparyani@gmail.com>
2015-08-17 19:13:50 -0700

    added files to source control

---
+++ JunksOut/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartupAttribute(typeof(JunksOut.Startup))]
+namespace JunksOut
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
